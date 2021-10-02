e97ab045629eee39b3abf23d0383f4e3e2a15a96
Mettra <Mettra@users.noreply.github.com>
2021-10-02 16:17:31 -0700

    More work on chatting, Delores will say Hello to you

--- Content/Web/Chat/script.js
+++ Content/Web/Chat/script.js
@@ -1,125 +1,164 @@
-let People = []
-
-function addPerson(name, icon) {
-  let person = {
-    name: name,
-    displayName: name,
-    icon: "images/" + icon + ".png",
-    messages: []
-  }
-
-  People.push(person)
-  return person
-}
-
-function addMessage(personMessage, fromPerson, message, time) {
-  if(typeof personMessage === "string") {
-    personMessage = People.find((p) => {return p.name == personMessage} )
-  }
-
-  if(typeof fromPerson === "string") {
-    fromPerson = People.find((p) => {return p.name == fromPerson} )
-  }
-
-  let msg = {
-    from: fromPerson,
-    text: message,
-    time: time
-  }
-  personMessage.messages.push(msg)
-  return msg
-}
-
-let you = addPerson("Jogn Idogun", "default_icon");
-you.displayName = "Jogn Idogun (You)"
-
-addPerson("Sally", "default_icon")
-addPerson("Joe", "default_icon")
-addPerson("Mary", "default_icon")
-
-addMessage("Sally", "Sally", "Hello!", "now")
-
-/* For later
-
-  <div class="quoted-file">
-    <div class="preparatory-text">
-      <p>Post</p>
-      <i class="fas fa-sort-down"></i>
-    </div>
-    <div class="file-figure">
-      <i class="far fa-file-alt fa-2x"></i>
-      <div class="file-detail">
-        <h5>1/9 Meeting Notes</h5>
-        <small>Last edited just now</small>
-      </div>
-    </div>
-  </div>
-
-
-
-  <div class="quoted">
-    <h5>Team status meeting</h5>
-    <p class="quoted-text">Today from 1:00PM to 1:30PM</p>
-  </div>
-
-
-  <span class="mention">@lisa</span>
-  */
-
-
- function createMessageHTML(fromPerson, message, time) {
-  time = "10:00pm";
-
-  return `
-  <article class="feed">
-    <section class="feeds-user-avatar">
-      <img src="${fromPerson.icon}" alt="${fromPerson.name}" width="40" />
-    </section>
-    <section class="feed-content">
-      <section class="feed-user-info">
-        <h4>${fromPerson.name} <span class="time-stamp">${time}</span></h4>
-      </section>
-      <div>
-        <p class="feed-text">
-          ${message}
-        </p>
-      </div>
-    </section>
-  </article>
-  `
-}
-
-let SelectedPerson = People[0]
-function switchToChat(id) {
-  let person = People[id]
-  SelectedPerson = person
-
-  let header = document.getElementById("channel-name")
-  header.innerHTML = person.displayName
-
-  let header_avatar = document.getElementById("channel-avatar")
-  header_avatar.innerHTML = `<img src="${person.icon}" alt="${person.name}" width="40" />`
-
-  let mainfeed = document.getElementById("mainfeed")
-  mainfeed.innerHTML = ""
-
-  for(let msg of person.messages) {
-    mainfeed.innerHTML += createMessageHTML(msg.from, msg.text, msg.time)
-  }
-
-  //Add the selected tab class to the right one
-  let dm_contacts = document.getElementById("dm_contacts")
-  for(let c of dm_contacts.children) {
-    c.classList.remove("direct-messages-selected")
-  }
-
-  let contactNode = document.getElementById(`dm_contact_${id}`)
-  contactNode.classList.add("direct-messages-selected")
-
-}
-
-(function() 
+(async () => 
 {
+  //For web debugging
+  if(typeof CefSharp === "undefined") {
+    CefSharp = {}
+    CefSharp.BindObjectAsync = () => {}
+
+    game = {}
+    game.readFile = (path) => {
+      return `[ "Hello!" ]`
+    }
+  }
+
+  await CefSharp.BindObjectAsync("game", "bound");
+
+
+  let People = []
+  
+
+  function addPerson(name, icon) {
+    let person = {
+      name: name,
+      displayName: name,
+      icon: "images/" + icon + ".png",
+      responses: [],
+      messages: []
+    }
+
+    game.readFile(`Content/Web/Chat/people/${name}/responses.json`).then((r) => {
+      if(r != "") {
+        person.responses = JSON.parse(r)
+      }
+    })
+
+    People.push(person)
+    return person
+  }
+
+  function addMessage(personMessage, fromPerson, message, time) {
+    if(typeof personMessage === "string") {
+      personMessage = People.find((p) => {return p.name == personMessage} )
+    }
+
+    if(typeof fromPerson === "string") {
+      fromPerson = People.find((p) => {return p.name == fromPerson} )
+    }
+
+    let msg = {
+      from: fromPerson,
+      text: message,
+      time: time
+    }
+    personMessage.messages.push(msg)
+    return msg
+  }
+
+  let you = addPerson("Jogn Idogun", "default_icon");
+  you.displayName = "Jogn Idogun (You)"
+
+  addPerson("Delores", "default_icon")
+  addPerson("Sally", "default_icon")
+  addPerson("Joe", "default_icon")
+  addPerson("Mary", "default_icon")
+
+  addMessage("Sally", "Sally", "Hello!", "now")
+
+
+  let SelectedPerson = People[0]
+
+  /* For later
+
+    <div class="quoted-file">
+      <div class="preparatory-text">
+        <p>Post</p>
+        <i class="fas fa-sort-down"></i>
+      </div>
+      <div class="file-figure">
+        <i class="far fa-file-alt fa-2x"></i>
+        <div class="file-detail">
+          <h5>1/9 Meeting Notes</h5>
+          <small>Last edited just now</small>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="quoted">
+      <h5>Team status meeting</h5>
+      <p class="quoted-text">Today from 1:00PM to 1:30PM</p>
+    </div>
+
+
+    <span class="mention">@lisa</span>
+    */
+
+
+  function createMessageHTML(fromPerson, message, time) {
+    time = "10:00pm";
+
+    return `
+    <article class="feed">
+      <section class="feeds-user-avatar">
+        <img src="${fromPerson.icon}" alt="${fromPerson.name}" width="40" />
+      </section>
+      <section class="feed-content">
+        <section class="feed-user-info">
+          <h4>${fromPerson.name} <span class="time-stamp">${time}</span></h4>
+        </section>
+        <div>
+          <p class="feed-text">
+            ${message}
+          </p>
+        </div>
+      </section>
+    </article>
+    `
+  }
+
+  function sendMessage(text) {
+    let mainfeed = document.getElementById("mainfeed")
+
+    let toPerson = SelectedPerson
+    let msg = addMessage(toPerson, you, text, "now")
+    mainfeed.innerHTML += createMessageHTML(msg.from, msg.text, msg.time)
+
+    if(toPerson.responses.length > 0) {
+      let response_msg = addMessage(toPerson, toPerson, toPerson.responses[0], "now")
+      mainfeed.innerHTML += createMessageHTML(response_msg.from, response_msg.text, response_msg.time)
+    }
+
+    mainfeed.scrollTop = mainfeed.scrollHeight - mainfeed.clientHeight;
+  }
+
+  window.switchToChat = (id) => {
+    let person = People[id]
+    SelectedPerson = person
+
+    let header = document.getElementById("channel-name")
+    header.innerHTML = person.displayName
+
+    let header_avatar = document.getElementById("channel-avatar")
+    header_avatar.innerHTML = `<img src="${person.icon}" alt="${person.name}" width="40" />`
+
+    let mainfeed = document.getElementById("mainfeed")
+    mainfeed.innerHTML = ""
+
+    for(let msg of person.messages) {
+      mainfeed.innerHTML += createMessageHTML(msg.from, msg.text, msg.time)
+    }
+
+    //Add the selected tab class to the right one
+    let dm_contacts = document.getElementById("dm_contacts")
+    for(let c of dm_contacts.children) {
+      c.classList.remove("direct-messages-selected")
+    }
+
+    let contactNode = document.getElementById(`dm_contact_${id}`)
+    contactNode.classList.add("direct-messages-selected")
+  }
+
   let dm_contacts = document.getElementById("dm_contacts")
 
   dm_contacts.innerHTML = ""
@@ -143,10 +182,7 @@ function switchToChat(id) {
         e.preventDefault();
 
         if(textBox.textContent != "") {
-          let msg = addMessage(SelectedPerson, you, textBox.textContent, "now")
-
-          let mainfeed = document.getElementById("mainfeed")
-          mainfeed.innerHTML += createMessageHTML(msg.from, msg.text, msg.time)
+          sendMessage(textBox.textContent)
           textBox.textContent = ""
         }
     }
--- Scripts/ChatForm.cs
+++ Scripts/ChatForm.cs
@@ -1,14 +1,29 @@
 ﻿using CefSharp;
 using CefSharp.SchemeHandler;
+using CefSharp.Web;
 using CefSharp.WinForms;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
 namespace MonoGame_Core.Scripts
 {
+    public class CallbackObjectForJs
+    {
+        public string readFile(string path)
+        {
+            if(File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+
+            return "";
+        }
+    }
+
     public class ChatForm : NoCloseForm
     {
         public ChatForm()
@@ -32,6 +47,24 @@ namespace MonoGame_Core.Scripts
             //Create a new instance in code or add via the designer
             //Set the ChromiumWebBrowser.Address property to your Url if you use the designer.
             var browser = new ChromiumWebBrowser("localfolder://cefsharp/");
+
+            browser.ConsoleMessage += (sender, args) =>
+            {
+                Debug.WriteLine(string.Format("Webview {0}({1}): {2}",
+                                                args.Source,
+                                                args.Line,
+                                                args.Message));
+            };
+
+            browser.JavascriptObjectRepository.ResolveObject += (sender, e) =>
+            {
+                var repo = e.ObjectRepository;
+                if (e.ObjectName == "game")
+                {
+                    repo.Register("game", new CallbackObjectForJs());
+                }
+            };
+
             this.Controls.Add(browser);
         }
     }
