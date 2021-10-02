ea18d84b351d8c3c8a0279c1dc5f86c3516343ab
Mettra <Mettra@users.noreply.github.com>
2021-10-01 21:42:28 -0700

    Default to MainMenu

--- Scripts/Managers/GameManager.cs
+++ Scripts/Managers/GameManager.cs
@@ -32,7 +32,7 @@ namespace MonoGame_Core.Scripts
             CollisionManager.Initilize();
             CoroutineManager.Initilize();
             CameraManager.Initilize();
-            SceneManager.Initilize(Content, new ChatWindowScene());
+            SceneManager.Initilize(Content, new MainMenu());
 
             base.Initialize();
         }
