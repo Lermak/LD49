0df1778aa5e1d4491d2305dae94ce6dc7b8022da
Alexander Wilkinson <awrdp@comcast.net>
2021-10-03 00:38:14 -0700

    new dial art stuff

--- Scripts/Scenes/NuclearScene.cs
+++ Scripts/Scenes/NuclearScene.cs
@@ -27,18 +27,16 @@ namespace MonoGame_Core.Scripts
             CameraManager.Cameras[0].SetMinPos(size/2 * -1);
             CameraManager.Cameras[0].SetMaxPos(size/2);
             
-
+            // Textures
             ResourceManager.Textures = new Dictionary<string, Texture2D>();
-            //NuclearButton!
             ResourceManager.Textures["CoolantButton"] = Content.Load<Texture2D>("Images/Nuclear/button");
             ResourceManager.Textures["CoolantButtonHover"] = Content.Load<Texture2D>("Images/Nuclear/button_hover");
             ResourceManager.Textures["CoolantButtonPress"] = Content.Load<Texture2D>("Images/Nuclear/button_press");
-            //Dial
             ResourceManager.Textures["DialArrow"] = Content.Load<Texture2D>("Images/Nuclear/dial_arrow");
             ResourceManager.Textures["DialBack"] = Content.Load<Texture2D>("Images/Nuclear/dial_back");
             ResourceManager.Textures["DialBorder"] = Content.Load<Texture2D>("Images/Nuclear/dial_border");
-            //Scene background
-            ResourceManager.Textures["BG"] = Content.Load<Texture2D>("Images/Background");
+            ResourceManager.Textures["HeatText"] = Content.Load<Texture2D>("Images/Nuclear/heat_text");
+            ResourceManager.Textures["BG"] = Content.Load<Texture2D>("Images/Nuclear/background");
 
             ResourceManager.SoundEffects["alert"] = Content.Load<SoundEffect>("Sound/alert");
             SoundManager.SoundEffects["alert"] = ResourceManager.SoundEffects["alert"].CreateInstance();
@@ -46,21 +44,22 @@ namespace MonoGame_Core.Scripts
             Vector2 dialSize = new Vector2(200, 200);
             GameObjects = new List<GameObject>();
 
-            WorldObject dialBackObj = new WorldObject("DialBack", "DialBG", dialSize, screenCenter, 1);
-            NuclearDial dialArrowObj = new NuclearDial("DialArrow", "NuclearDial", dialSize, screenCenter, 2);
-            WorldObject dialBorderObj = new WorldObject("DialBorder", "DialBorder", dialSize, screenCenter, 3);
+            GameObjects.Add(new WorldObject("BG", "Background", new Vector2(250, 250), screenCenter, 0));
+
+            GameObjects.Add(new WorldObject("HeatText", "HeatText", new Vector2(96, 32), screenCenter + new Vector2(0, 86), 1));
+
+            Vector2 buttonCenterPos = screenCenter + new Vector2(0, -32);
+
+            WorldObject dialBackObj = new WorldObject("DialBack", "DialBG", dialSize, buttonCenterPos, 1);
+            NuclearDial dialArrowObj = new NuclearDial("DialArrow", "NuclearDial", dialSize, buttonCenterPos, 2);
+            WorldObject dialBorderObj = new WorldObject("DialBorder", "DialBorder", dialSize, buttonCenterPos, 3);
             dialArrowObj.Transform.AttachToTransform(dialBackObj.Transform);
             GameObjects.Add(dialBackObj);
             GameObjects.Add(dialArrowObj);
             GameObjects.Add(dialBorderObj);
 
-            GameObjects.Add(new WorldObject("BG", "Background", new Vector2(1920, 1080),screenCenter, 0));
-            ((WorldObject)GameObjects[GameObjects.Count - 1]).SpriteRenderer.Transform.Layer = 0;
-
             Vector2 cooldownButtonSize = new Vector2(100, 100);
-            //Wonky, yell at Rhen later
-            Vector2 ButtonPosition = screenCenter;
-            Button cooldownButton = new Button("CoolantButton", "CoolantButtonHover", "NuclearButton", cooldownButtonSize, ButtonPosition, 4, () =>
+            Button cooldownButton = new Button("CoolantButton", "CoolantButtonHover", "NuclearButton", cooldownButtonSize, buttonCenterPos, 4, () =>
             {
                 if (!NuclearLevel.started)
                     NuclearLevel.started = true;
