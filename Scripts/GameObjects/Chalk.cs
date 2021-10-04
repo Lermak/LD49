3b72e5a839563878c028f5377990838b54c2c0c9
Rhen <rhenniles@yahoo.com>
2021-10-04 01:23:54 -0400

    update

---
+++ Scripts/GameObjects/Chalk.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame_Core.Scripts
+{ 
+    public class Chalk : WorldObject
+    {
+        public Chalk(Vector2 pos) : base("Chalk")
+    }
+}
